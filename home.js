07614de095551f968bfd70c88314067f7b1e61fb
jaime <garciadiazjaime@gmail.com>
2016-02-03 20:33:09 -0600

    init

---
+++ home.js
@@ -0,0 +1,399 @@
+/* eslint max-len: [2, 500, 4] */
+
+export default {
+  block1: {
+    texts: {
+      text1: 'LA CLÍNICA DE LA PIEL',
+      text2: 'La clínica',
+    },
+    links: {
+        link1: {
+          href: '/la-clinica',
+          title: ''
+        },
+      },
+    images: {
+      image1: {
+        src: '',
+        alt: 'la clinica de la piel',
+        },
+      },
+   },
+
+  block2: {
+    texts: {
+      text1: 'Nuestros Servicios',
+      text2: 'Dermatología Cosmetíca',
+      text3: 'Procedimientos de alta tecnología para el cuidado de la piel.',
+      text4: 'Dermatología Clínica',
+      text5: 'Tratamientos para enfermedades de la piel.',
+      text6: 'NUESTROS DOCTORES',
+      text7: 'Dermatólogos Certificados',
+      text8: 'Doctores certificados por el consejo mexicano de dermatología.',
+      text9: 'NUESTRAS',
+      text10: 'Ubicaciones',
+      text11: 'Tijuana',
+      text12: 'Ensenada',
+      text13: 'Mexicali',
+      text14: 'LA SIGUIENTE GENERACIÓN EN DERMATOLOGÍA',
+      },
+    links: {
+      link1: {
+        href: '/servicios/dermatologia-certificados',
+        title: ''
+        },
+      link2: {
+        href: '/servicios/dermatologia-cosmetica',
+        title: ''
+        },
+      link3: {
+        href: '/equipo/dermatologos-clinica',
+        title: ''
+        },
+      link4: {
+        href: '/ubicacion/tijuana',
+        title: 'Tijuana'
+        },
+      link5: {
+        href: '/ubicacion/ensenada',
+        title: 'Ensenada'
+        },
+      link6: {
+        href: '/ubicacion/mexicali',
+        title: 'Mexicali'
+        },
+      },
+    images: {
+      image1: {
+        src: '',
+        alt: 'dermatologos sertificados',
+        },
+      image2: {
+        src: '',
+        alt: 'dermatologia cosmetica',
+       },
+     },
+   },
+
+  block3: {
+    texts: {
+      text1: 'Contamos con una amplia variedad de productos para tratamientos dermatológicos, cremas y cuidado de la piel.',
+      text2: 'En Tijuana:',
+      text3: '(664) 634-1615',
+      text4: 'LLAMANÓS',
+      text5: 'Visítanos en:',
+      text6: 'Blvd Sánchez Taboada 101102-15, Plaza Financiera, Zona Río, Tijuana, B.C.',
+    },
+    links: {
+      link1: {
+        href: '/contacto',
+        title: 'LLÁMANOS'
+      },
+    },
+    images: {
+      image1: {
+        src: '',
+        alt: 'laser medica',
+        },
+      image2: {
+        src: '',
+        alt: 'lasermedica cremas ',
+        },
+      },
+   },
+
+  block4: {
+    images: {
+      image1: {
+        src: '',
+        alt: 'la roche posay',
+        },
+      image2: {
+        src: '',
+        alt: 'avene',
+        },
+      image3: {
+        src: '',
+        alt: 'vichy',
+        },
+      image4: {
+        src: '',
+        alt: 'cetaphil',
+        },
+      image5: {
+        src: '',
+        alt: 'a derma',
+        },
+     },
+  },
+
+  block5: {
+    texts: {
+      text1: 'TRATAMIENTOS',
+      text2: 'Dermatológicos',
+      text3: 'Depilación Laser ',
+      text4: 'tratamiento 360',
+      text5: 'Acné',
+      text6: 'Melasma',
+      }
+    links: {
+      link1: {
+        href: '/servicios/depilacion-laser',
+        title: 'depilacion laser',
+        },
+      link2: {
+        href: '/servicios/tratamiento-360',
+        title: 'tratamiento 360',
+        },
+      link3: {
+        href: '/servicios/acne',
+        title: 'acné',
+        },
+      link4: {
+        href: '/servicios/melasma',
+        title: 'melasma',
+        },
+      },
+    images: {
+      image1: {
+        src: '',
+        alt: 'depilacion laser',
+        },
+      image2: {
+        src: '',
+        alt: 'tratamiento 360',
+        },
+      image3: {
+        src: '',
+        alt: 'acne',
+        },
+      image4: {
+        src: '',
+        alt: 'melasma',
+        },
+      },
+   },
+
+
+
+
+
+  block1: {
+    texts: {
+      text1: 'MÁS DE 30 AÑOS DE EXPERIENCIA',
+      text2: 'La clínica',
+    },
+    links: {
+      link1: {
+        href: '/la-clinica',
+        title: ''
+      },
+    }
+    images: {
+      image1: {
+        src: '',
+        alt: 'mas de 30 anos de experiencia',
+      },
+    },
+  },
+
+  block2: {
+    texts: {
+      text1: 'LA CLÍNICA',
+      text2: 'Quiénes Somos',
+      text3: 'Lasermedica fue fundada en la ciudad de Tijuana hace más de 30 años por el Doctor Jorge Alberto Ibarra Ibarra, quien al egresar de la Universidad Autónoma de Guadalajara vio en la localidad la necesidad de una clínica dermatológica seria y preocupada por la salud de la piel; Aunado a lo anterior, su dedicación por el estudio de las enfermedades de la piel y su tratamiento lo ha llevado a ser una de las eminencias en cuanto a la cosmetología clínica en México.',
+      text4: 'A través de nuestra especialización en dermatología, cosmetología y cirugía dermatológica en Lasermedica nos avocamos a una atención única en el cuidado de la piel avalándonos en nuestra experiencia y buscando las técnicas más novedosas - y sobre todo eficientes - para lograr los mejores resultados en nuestros pacientes.',
+      text5: 'Atención única en el cuidado de la piel',
+    },
+    images: {
+      image1: {
+        src: '',
+        alt: 'atencion unica en el cuidado de la piel',
+      },
+    },
+  },
+
+  block3: {
+    texts: {
+      text1: 'LA SIGUIENTE GENERACIÓN EN DERMATOLOGÍA',
+    },
+    images: {
+      image1: {
+        src: '',
+        alt: 'la siguiente generacion en dermatologia',
+      },
+    },
+  },
+
+  block4: {
+    texts: {
+      text1: 'NUESTRA MISIÓN',
+      text2: 'Nuestro Compromiso',
+      text3: 'En Lasermedica nos dedicamos al cuidado y tratamiento de enfermedades de la piel así como al mejoramiento de su apariencia; nuestro objetivo es que nuestros pacientes luzcan una piel saludable y radiante, lo que conseguimos a través de la actualización constante de nuestros especialistas, quienes se preocupan por ofrecer el mejor servicio con un trato cálido, responsable y profesional.',
+      text4: 'NUESTROS VALORES',
+      text5: 'Experiencia',
+      text6: 'Confiabiliad',
+      text7: 'Honestidad',
+      text8: 'Responsabilidad',
+      text9: 'Compromiso',
+      },
+    images: {
+      image1: {
+        src: '',
+        alt: 'nuestra mision',
+      },
+    },
+  },
+
+  block5: {
+    texts: {
+      text1: 'NUESTRA VISIÓN',
+      text2: 'Nuestro futuro',
+      text3: 'Seguir consolidados en la región como la clínica dermatológica más reconocida por su labor profesional y altruista, así como mantenernos actualizados para seguir ofreciendo a nuestros clientes los mejores y más innovadores tratamientos dermatológicos.',
+      text4: 'AGENDAR CITA',
+      },
+    links: {
+      link1: {
+        href: 'contacto/agendar-cita',
+        title: 'AGENDAR CITA',
+        },
+      },
+    images: {
+      image1: {
+        src: '',
+        alt: 'doctores agendar cita',
+        },
+      },
+    },
+
+  block6: {
+    texts: {
+      text1: 'Conoce nuestros servicios y descubre cómo podemos ayudarte.',
+      text2: 'VER SERVICIOS',
+      text3: 'Conoce nuestras Ubicaciones',
+      text4: 'Tijuana',
+      text5: 'Ensenada',
+      text6: 'Mexicali',
+      }
+    links: {
+      link1: {
+        href: 'servicios/ver-servicios',
+        title: 'VER SERVICIOS ''
+        },
+      link4: {
+        href: '/ubicacion/tijuana',
+        title: 'Tijuana'
+        },
+      link5: {
+        href: '/ubicacion/ensenada',
+        title: 'Ensenada'
+        },
+      link6: {
+        href: '/ubicacion/mexicali',
+        title: 'Mexicali'
+        },
+      }
+    images: {
+      image1: {
+        src: '',
+        alt: 'conoce nuestras ubicaciones',
+        },
+      },
+    },
+
+
+
+
+
+
+  block1: {
+    texts: {
+      text1: 'DERMATÓLOGOS CERTIFICADOS',
+      text2: 'Nuestro Equipo',
+      },
+    links: {
+      link1: {
+        href: '/nuestro equipo',
+        title: '',
+        },
+      }
+    images: {
+      image1: {
+        src: '',
+        alt: 'nuestro equipo',
+        },
+      },
+    },
+
+  block2: {
+    texts: {
+      text1: 'La siguiente generación en la dermatología',
+      text2: 'Dr. Jorge Alberto Ibarra',
+      text3: 'DERMATÓLOGO',
+      text4: 'Visionario y precursor de la dermatología en México',
+      text5: 'En Lasermedica contamos con un conjunto de médicos profesionales ampliamente capacitados y calificados en su rubro; tal es el caso de nuestro fundador el Dr. Jorge Alberto Ibarra Ibarra, quien a lo largo de más de 30 años al egresar de la Universidad de Guadalajara y cursar el posgrado en Dermatología en el Instituto Dermatológico de Jalisco, comenzó una carrera llena de éxitos como visionario y precursor de la dermatología en el país; dentro de su labor como médico ha destacado como Expresidente del Colegio Médico de Dermatólogos, pertenece a la Academia Mexicana de Dermatología y a la Sociedad Mexicana de Dermatología.',
+      text6: 'Así mismo, dentro del currículum del Dr. Jorge Ibarra se encuentra el ser parte del Colegio Médico de Dermatólogos de Jalisco y de la Sociedad de Dermatología A.C., entidades importantes para el desarrollo de la dermatología en México. El Dr. Ibarra es miembro activo en la prestigiada International Society of Dermatologic Surgery con sede en Suiza, en la cual se realizan investigaciones para obtener los tratamientos más innovadores en cuanto a cirugía dermatológica. Por otro lado, también pertenece al Colegio Ibero Latino Americano de Dermatología en el que se busca la actualización en enfermedades dermatológicas y su cura.'
+      text7: 'Actualmente el Doctor Jorge Ibarra desempeña también la labor de catedrático en la Universidad Autónoma de Baja California Campus Tijuana en la facultad de medicina, dando a sus alumnos las bases de la dermatología, inculcándoles la pasión y dedicación por la materia.'
+      text8: 'La siguiente generación en dermatología'
+      text9: 'Dra. Paulina Priscilla Ibarra Ramírez'
+      text10: 'DERMATÓLOGA'
+      text11: 'Miembro activo de la Academia Mexicana de Dermatología'
+      text12: 'Como titular en Cirugía Dermatológica tenemos a la Dra.Paulina Priscilla Ibarra Ramírez, médica dermatóloga egresada de la Universidad Autónoma de Baja California, quien cursó el internado en el Hospital civil Antiguo de Guadalajara, y realizó la especialidad de Dermatología en el Instituto Dermatológico de Jalisco “Dr. José Barba Rubio” con la sub especialidad en Cirugía Dermatológica.'
+      text13: 'Como parte esencial a su crecimiento profesional, la Dra. Ibarra ha recibido entrenamiento en Cirugía Láser en Málaga, España y en Dermatología cosmética en Buenos Aires Argentina. Así mismo es miembro activo de Colegio de Dermatólogos de Tijuana, Colegio Médico de dermatólogos de Jalisco, Sociedad de Dermatología A.C., Academia Mexicana de Dermatología y el Colegio Ibero latinoamericano de Dermatología; La Dr. Paulina Ibarra también desempeña labores académicas al impartir la asignatura de Dermatología en la Facultad de Medicina en la Universidad Autónoma de Baja California.'
+      },
+    images: {
+      image1: {
+        src: '',
+        alt: 'Dr. Jorge Alberto Ibarra',
+      },
+      image2: {
+        src: '',
+        alt: 'Dra. Paulina Priscilla Ibarra Ramírez',
+      },
+    },
+  },
+
+  block3: {
+    texts: {
+      text1: 'En Lasermedica contamos también con un equipo de médicos profesionales que lo guiarán durante su tratamiento, quienes ayudarán a contestar sus dudas y tratamiento a seguir.',
+      },
+    images: {
+      image1: {
+        src: '',
+        alt: 'en lasermedica contamos',
+        },
+      },
+    },
+
+  block4: {
+    texts: {
+      text1: 'Ana Luisa Encinas',
+      text2: 'Cosmetóloga',
+      text3: 'Dr. Jorge Varela',
+      text4: 'Cirugía Laser y Medicina estética',
+      text5: 'Dr. Carlos Rojo',
+      text6: 'Cirugía Laser',
+      text7: 'Conoce nuestros servicios y descubre cómo podemos ayudarte.',
+      text8: 'VER SERVICIOS',
+        },
+    links: {
+      link1: {
+        href: '/ver-servicios',
+        title: 'ver servicios',
+        },
+      }
+    images: {
+      image1: {
+        src: '',
+        alt: 'Ana Luisa Encinas',
+          },
+      image2: {
+        src: '',
+        alt: 'Dr. Jorge Varela',
+          },
+      image3: {
+        src: '',
+        alt: 'Dr. Carlos Rojo',
+        },
+      },
+    },
