05dd60f68a594ca23c1139f3495445c8ff088451
ari <garciadiazivanari@gmail.com>
2016-02-09 12:13:26 -0800

    agregar data en about us

--- src/shared/components/sections/about/data.js
+++ src/shared/components/sections/about/data.js
@@ -2,124 +2,124 @@
 
 export default {
   block1: {
-  texts: {
-    text1: 'MÁS DE 30 AÑOS DE EXPERIENCIA',
-    text2: 'La clínica',
-  },
-  links: {
-    link1: {
-      href: '/la-clinica',
-      title: ''
+    texts: {
+      text1: 'MÁS DE 30 AÑOS DE EXPERIENCIA',
+      text2: 'La clínica',
     },
-  },
-  images: {
-    image1: {
-      src: '',
-      alt: 'mas de 30 anos de experiencia',
-    },
-  },
-},
-
-block2: {
-  texts: {
-    text1: 'LA CLÍNICA',
-    text2: 'Quiénes Somos',
-    text3: 'Lasermedica fue fundada en la ciudad de Tijuana hace más de 30 años por el Doctor Jorge Alberto Ibarra Ibarra, quien al egresar de la Universidad Autónoma de Guadalajara vio en la localidad la necesidad de una clínica dermatológica seria y preocupada por la salud de la piel; Aunado a lo anterior, su dedicación por el estudio de las enfermedades de la piel y su tratamiento lo ha llevado a ser una de las eminencias en cuanto a la cosmetología clínica en México.',
-    text4: 'A través de nuestra especialización en dermatología, cosmetología y cirugía dermatológica en Lasermedica nos avocamos a una atención única en el cuidado de la piel avalándonos en nuestra experiencia y buscando las técnicas más novedosas - y sobre todo eficientes - para lograr los mejores resultados en nuestros pacientes.',
-    text5: 'Atención única en el cuidado de la piel',
-  },
-  images: {
-    image1: {
-      src: '',
-      alt: 'atencion unica en el cuidado de la piel',
-    },
-  },
-},
-
-block3: {
-  texts: {
-    text1: 'LA SIGUIENTE GENERACIÓN EN DERMATOLOGÍA',
-  },
-  images: {
-    image1: {
-      src: '',
-      alt: 'la siguiente generacion en dermatologia',
-    },
-  },
-},
-
-block4: {
-  texts: {
-    text1: 'NUESTRA MISIÓN',
-    text2: 'Nuestro Compromiso',
-    text3: 'En Lasermedica nos dedicamos al cuidado y tratamiento de enfermedades de la piel así como al mejoramiento de su apariencia; nuestro objetivo es que nuestros pacientes luzcan una piel saludable y radiante, lo que conseguimos a través de la actualización constante de nuestros especialistas, quienes se preocupan por ofrecer el mejor servicio con un trato cálido, responsable y profesional.',
-    text4: 'NUESTROS VALORES',
-    text5: 'Experiencia',
-    text6: 'Confiabiliad',
-    text7: 'Honestidad',
-    text8: 'Responsabilidad',
-    text9: 'Compromiso',
-    },
-  images: {
-    image1: {
-      src: '',
-      alt: 'nuestra mision',
-    },
-  },
-},
-
-block5: {
-  texts: {
-    text1: 'NUESTRA VISIÓN',
-    text2: 'Nuestro futuro',
-    text3: 'Seguir consolidados en la región como la clínica dermatológica más reconocida por su labor profesional y altruista, así como mantenernos actualizados para seguir ofreciendo a nuestros clientes los mejores y más innovadores tratamientos dermatológicos.',
-    text4: 'AGENDAR CITA',
-    },
-  links: {
-    link1: {
-      href: 'contacto/agendar-cita',
-      title: 'AGENDAR CITA',
+    links: {
+      link1: {
+        href: '/la-clinica',
+        title: '',
       },
     },
-  images: {
-    image1: {
-      src: '',
-      alt: 'doctores agendar cita',
+    images: {
+      image1: {
+        src: '',
+        alt: 'mas de 30 anos de experiencia',
       },
     },
   },
 
-block6: {
-  texts: {
-    text1: 'Conoce nuestros servicios y descubre cómo podemos ayudarte.',
-    text2: 'VER SERVICIOS',
-    text3: 'Conoce nuestras Ubicaciones',
-    text4: 'Tijuana',
-    text5: 'Ensenada',
-    text6: 'Mexicali',
+  block2: {
+    texts: {
+      text1: 'LA CLÍNICA',
+      text2: 'Quiénes Somos',
+      text3: 'Lasermedica fue fundada en la ciudad de Tijuana hace más de 30 años por el Doctor Jorge Alberto Ibarra Ibarra, quien al egresar de la Universidad Autónoma de Guadalajara vio en la localidad la necesidad de una clínica dermatológica seria y preocupada por la salud de la piel; Aunado a lo anterior, su dedicación por el estudio de las enfermedades de la piel y su tratamiento lo ha llevado a ser una de las eminencias en cuanto a la cosmetología clínica en México.',
+      text4: 'A través de nuestra especialización en dermatología, cosmetología y cirugía dermatológica en Lasermedica nos avocamos a una atención única en el cuidado de la piel avalándonos en nuestra experiencia y buscando las técnicas más novedosas - y sobre todo eficientes - para lograr los mejores resultados en nuestros pacientes.',
+      text5: 'Atención única en el cuidado de la piel',
     },
-  links: {
-    link1: {
-      href: 'servicios/ver-servicios',
-      title: 'VER SERVICIOS ',
-    },
-    link4: {
-      href: '/ubicacion/tijuana',
-      title: 'Tijuana'
-      },
-    link5: {
-      href: '/ubicacion/ensenada',
-      title: 'Ensenada'
-      },
-    link6: {
-      href: '/ubicacion/mexicali',
-      title: 'Mexicali'
+    images: {
+      image1: {
+        src: '',
+        alt: 'atencion unica en el cuidado de la piel',
       },
     },
-  images: {
-    image1: {
-      src: '',
-      alt: 'conoce nuestras ubicaciones',
+  },
+
+  block3: {
+    texts: {
+      text1: 'LA SIGUIENTE GENERACIÓN EN DERMATOLOGÍA',
+    },
+    images: {
+      image1: {
+        src: '',
+        alt: 'la siguiente generacion en dermatologia',
+      },
+    },
+  },
+
+  block4: {
+    texts: {
+      text1: 'NUESTRA MISIÓN',
+      text2: 'Nuestro Compromiso',
+      text3: 'En Lasermedica nos dedicamos al cuidado y tratamiento de enfermedades de la piel así como al mejoramiento de su apariencia; nuestro objetivo es que nuestros pacientes luzcan una piel saludable y radiante, lo que conseguimos a través de la actualización constante de nuestros especialistas, quienes se preocupan por ofrecer el mejor servicio con un trato cálido, responsable y profesional.',
+      text4: 'NUESTROS VALORES',
+      text5: 'Experiencia',
+      text6: 'Confiabiliad',
+      text7: 'Honestidad',
+      text8: 'Responsabilidad',
+      text9: 'Compromiso',
+    },
+    images: {
+      image1: {
+        src: '',
+        alt: 'nuestra mision',
+      },
+    },
+  },
+
+  block5: {
+    texts: {
+      text1: 'NUESTRA VISIÓN',
+      text2: 'Nuestro futuro',
+      text3: 'Seguir consolidados en la región como la clínica dermatológica más reconocida por su labor profesional y altruista, así como mantenernos actualizados para seguir ofreciendo a nuestros clientes los mejores y más innovadores tratamientos dermatológicos.',
+      text4: 'AGENDAR CITA',
+    },
+    links: {
+      link1: {
+        href: 'contacto/agendar-cita',
+        title: 'AGENDAR CITA',
+      },
+    },
+    images: {
+      image1: {
+        src: '',
+        alt: 'doctores agendar cita',
+      },
+    },
+  },
+
+  block6: {
+    texts: {
+      text1: 'Conoce nuestros servicios y descubre cómo podemos ayudarte.',
+      text2: 'VER SERVICIOS',
+      text3: 'Conoce nuestras Ubicaciones',
+      text4: 'Tijuana',
+      text5: 'Ensenada',
+      text6: 'Mexicali',
+    },
+    links: {
+      link1: {
+        href: 'servicios/ver-servicios',
+        title: 'VER SERVICIOS ',
+      },
+      link4: {
+        href: '/ubicacion/tijuana',
+        title: 'Tijuana',
+      },
+      link5: {
+        href: '/ubicacion/ensenada',
+        title: 'Ensenada',
+      },
+      link6: {
+        href: '/ubicacion/mexicali',
+        title: 'Mexicali',
+      },
+    },
+    images: {
+      image1: {
+        src: '',
+        alt: 'conoce nuestras ubicaciones',
       },
     },
   },
