d89954600718b9ce15810b4dd05e4ccb3939bc07
jaime <garciadiazjaime@gmail.com>
2016-02-10 21:12:34 -0600

    team block3

--- src/shared/components/sections/team/block3/index.js
+++ src/shared/components/sections/team/block3/index.js
@@ -8,12 +8,12 @@ export default class Block3 extends React.Component {
   render() {
     const { texts } = this.props.data;
     return (<div className={style.wr4}>
-        <div className="container-fluid">
-          <div className={style.blockWrapper1}>
-            <h2>{texts.text1}</h2>
-          </div>
+      <div className="container-fluid">
+        <div className={style.blockWrapper1}>
+          <h2>{texts.text1}</h2>
         </div>
-      </div>);
+      </div>
+    </div>);
   }
 }
 
--- src/shared/components/sections/team/index.js
+++ src/shared/components/sections/team/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 // import Block1 from './block1';
-import Block2 from './block2';
-// import Block3 from './block3';
+// import Block2 from './block2';
+import Block3 from './block3';
 // import Block4 from './block4';
 // import Block5 from './block5';
 // <Block1 data={data.block1} />
+// <Block2 data={data.block2} />
 
-// <Block3 data={data.block3} />
 // <Block4 data={data.block4} />
 // <Block5 data={data.block5} />
 import data from './data';
@@ -16,8 +16,7 @@ import data from './data';
 export default class EquipoSection extends React.Component {
   render() {
     return (<div>
-      <Block2 data={data.block2} />
-
+      <Block3 data={data.block3} />
     </div>);
   }
 }
