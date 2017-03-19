9c1bacb3386425110722f2e613d7eaaf847dcd79
jaime <garciadiazjaime@gmail.com>
2017-03-19 17:32:03 -0500

    radiofrequency

---
+++ src/shared/components/sections/services/db/cosmetic-dermatology/services/radiofrequency.js
@@ -0,0 +1,38 @@
+/* eslint max-len: [2, 1000, 4] */
+import _ from 'lodash';
+import Common from '../../common';
+
+module.exports = [{
+  type: 'title',
+  text: 'Radiofrequency (Alma - Accent)',
+  className: '',
+}, {
+  type: 'image',
+  src: '/images/servicios/dermatologia-cosmetica/05_radiofrecuencia.jpg',
+  text: '¿Qué es?',
+}, Common.EN.item1, {
+  type: 'text',
+  className: 'normal_break',
+  text: [
+    'Facial radiofrequency is a novelty option used to combat aging through tightening the skin that has loosened over time and it\'s mainly used on the forehead, under the eyes, cheeks, jaw and neck.',
+    'It\'s a very simple treatment that can be applied to various areas in the body, also used in the neck and neckline. It is not painful, you may only feel some discomfort during the procedure.',
+  ],
+}, Common.EN.item5, {
+  type: 'list',
+  className: '',
+  items: [
+    'Tightening the skin',
+    'Decrease sagging skin in the face and jawline',
+    'Tension the skin on the forehead',
+    'Reduce neck wrinkles and fine lines',
+  ],
+}, Common.EN.item7, {
+  type: 'text',
+  className: 'normal_break',
+  text: [
+    'Radiofrequency is a non-invasive, non-aggressive system with instant recovery. This novelty technique works through the emission of heat which causes the facial tissue to regenerate. This small heat pulsations stimulate the production of collagen, toning and improving the contour of the face.',
+    'Depending on the type of skin and the patient\'s needs, our experts will determine the number of sessions required to achieve the desired results, so we invite you to have an assessment with our staff.',
+  ],
+}, _.assign({}, Common.EN.item4, {
+  href: '/contact/radiofrequency',
+})];
