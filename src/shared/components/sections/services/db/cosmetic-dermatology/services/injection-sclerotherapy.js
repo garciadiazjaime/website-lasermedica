c822509dda1857ea963ec057a0331bc6a5641fec
jaime <garciadiazjaime@gmail.com>
2017-03-19 17:49:05 -0500

    trans carboxiterapia

---
+++ src/shared/components/sections/services/db/cosmetic-dermatology/services/injection-sclerotherapy.js
@@ -0,0 +1,34 @@
+/* eslint max-len: [2, 1000, 4] */
+import _ from 'lodash';
+import Common from '../../common';
+
+module.exports = [{
+  type: 'title',
+  text: 'Injection Sclerotherapy',
+  className: '',
+}, {
+  type: 'image',
+  src: '/images/servicios/dermatologia-cosmetica/08_escleroterapia_inyectada.jpg',
+  text: 'Tratamiento 360',
+}, Common.EN.item1, {
+  type: 'text',
+  className: '',
+  text: 'Vascular sclerotherapy treats the appearance of varicose veins in the legs, face and torso. It\'s not merely an aesthetics procedure because in addition to reducing the appearance, and eventually eliminating, varicose veins, it improves circulation, helps minimize the tiredness and pain that varicose veins can produce and can prevent further surgery in extreme cases.',
+}, Common.EN.item5, {
+  type: 'list',
+  className: '',
+  items: [
+    'Remove varicose veins of face, extremities and torso',
+    'Improve the appearance of legs',
+    'Help minimize the pain caused by varicose veins',
+  ],
+}, Common.EN.item7, {
+  type: 'text',
+  className: 'normal_break',
+  text: [
+    'Each session lasts less than 1 hour; the treatment consists of injecting a sclerosing solution into the vein, which causes the blocked vein - varix - to diminish its color and eventually disappear. It is important to know that for sclerotherapy to be successful it is necessary to perform several sessions to obtain the best possible results.',
+    'Sclerotherapy does not require anesthesia and you can return to your normal activities as soon as the session has finished.',
+  ],
+}, _.assign({}, Common.EN.item4, {
+  href: '/contact/injection-sclerotherapy',
+})];
